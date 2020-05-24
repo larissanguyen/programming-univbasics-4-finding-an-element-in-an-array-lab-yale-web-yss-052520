a13115f45edfcda0c1043ee1b8396b5027767595
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-24 19:37:11 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   index = 0
-  while index < array.length
+  while index < array.length do
     if array[index] == value_to_find
       return index
     index += 1
